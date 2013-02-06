06bbc653fda117948726dd1f5274b1d8eea4be9b
Cristian Carreño <kiel@acid.cl>
2013-02-06 16:32:09 -0300

    Fix module misspelling into TbkRails

--- lib/tbk-rails/version.rb
+++ lib/tbk-rails/version.rb
@@ -1,8 +1,6 @@
-module Tbk
-  module Rails
-    module VERSION
-      TBK = "0.9.2"
-      GEM = "0.0.1"
-    end
+module TbkRails
+  module VERSION
+    TBK = "0.9.2"
+    GEM = "0.0.1"
   end
 end
