635f2b94b8c142726d03a2f1c9e570f82f11930f
Cristian Carreño <kiel@acid.cl>
2013-02-06 16:43:15 -0300

    Update model wrapper syntax

--- lib/generators/tbk-rails/install_generator.rb
+++ lib/generators/tbk-rails/install_generator.rb
@@ -1,80 +1,78 @@
 require 'rails/generators'
-module Tbk
-  module Rails
-    class InstallGenerator < Rails::Generators::Base
-      desc "Install WebPayController views and a controller template"
-      source_root File.expand_path("../templates", __FILE__)
+module TbkRails
+  class InstallGenerator < Rails::Generators::Base
+    desc "Install WebPayController views and a controller template"
+    source_root File.expand_path("../templates", __FILE__)
 
-      def install_tbk_webpay
-        mode = yes?("Would you like to create minimal views?")
+    def install_tbk_webpay
+      mode = yes?("Would you like to create minimal views?")
 
-        if mode
-          say("Installing minimal version of Tbk-rails")
-        else
-          say("Installing default version of Tbk-rails")
-        end
-
-        say("Installing controllers ...", :orange)
-        install_controllers
-        say("Ready!", :green)
-
-        say("Installing routes ...", :orange)
-        install_routes
-        say("Ready!", :green)
-
-        if mode
-          minimal
-        else
-          default
-        end
-
-        say("Adding gem dependency to Gemfile", :orange)
-        gem("tbk", "~> #{Tbk::Rails::VERSION::TBK}" )
-
-        say("Installing TBK ... ", :orange)
-        run("bundle install tbk")
-
-        say("Complete!", :green)
-
+      if mode
+        say("Installing minimal version of Tbk-rails")
+      else
+        say("Installing default version of Tbk-rails")
       end
 
-      private
+      say("Installing controllers ...", :orange)
+      install_controllers
+      say("Ready!", :green)
 
-      def minimal
-        install_views Dir.glob('views/minimal/webpay/*')
+      say("Installing routes ...", :orange)
+      install_routes
+      say("Ready!", :green)
+
+      if mode
+        minimal
+      else
+        default
       end
 
-      def default
-        install_views Dir.glob('views/default/webpay/*')
-      end
+      say("Adding gem dependency to Gemfile", :orange)
+      gem("tbk", "~> #{Tbk::Rails::VERSION::TBK}" )
 
-      def install_views list
-        list.each do |view|
-          file = file_name(view)
-          copy_file view, "app/views/webpay/#{file}"
-        end
-      end
+      say("Installing TBK ... ", :orange)
+      run("bundle install tbk")
 
-      def install_controllers
-        controllers = Dir.glob('controllers/webpay/*')
-
-        controllers.each do |controller|
-          file = file_name(controller)
-          copy_file controller, "app/controllers/#{file}"
-        end
-      end
-
-      def install_routes
-        route("post '/webpay/pay'")
-        route("post '/webpay/confirmation'")
-        route("get '/webpay/success'")
-        route("get '/webpay/failure'")
-      end
-
-      def file_name dir
-        dir.split("/").last
-      end
+      say("Complete!", :green)
 
     end
+
+    private
+
+    def minimal
+      install_views Dir.glob('views/minimal/webpay/*')
+    end
+
+    def default
+      install_views Dir.glob('views/default/webpay/*')
+    end
+
+    def install_views list
+      list.each do |view|
+        file = file_name(view)
+        copy_file view, "app/views/webpay/#{file}"
+      end
+    end
+
+    def install_controllers
+      controllers = Dir.glob('controllers/webpay/*')
+
+      controllers.each do |controller|
+        file = file_name(controller)
+        copy_file controller, "app/controllers/#{file}"
+      end
+    end
+
+    def install_routes
+      route("post '/webpay/pay'")
+      route("post '/webpay/confirmation'")
+      route("get '/webpay/success'")
+      route("get '/webpay/failure'")
+    end
+
+    def file_name dir
+      dir.split("/").last
+    end
+
   end
 end
